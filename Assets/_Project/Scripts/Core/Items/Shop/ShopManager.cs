6ae44a2188dfb86fe1c9c71f03a0e0d0395fb1a9
ErickooKun <ericnonger@gmail.com>
2026-03-29 09:46:48 +0900

    feat: implement shop functionality, refactor dialogue manager
    - refactor `DialogueManager` into 3 seperate components, `DialogueManager`, `DialogueUI`, `DialogueOptionController` to follow S.O.L.I.D principles.
    - refactor `EventBus` to handle more data parameters from interacted entity
    - link `shopttemPrefab` to NPC `shopList`
    - Link `shopItemPrefab` button to `InventoryManager` add button.

--- Assets/_Project/Scripts/Core/Items/Shop/ShopManager.cs
+++ Assets/_Project/Scripts/Core/Items/Shop/ShopManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
+using TMPro;
 
 public class ShopManager : MonoBehaviour
 {
@@ -39,11 +41,41 @@ public class ShopManager : MonoBehaviour
         }
     }
     
-    private void SetupShop(string dialogueEvent)
+    private void SetupShop(string dialogueEvent, object data)
     {
         if (dialogueEvent == "ShopOpen")
         {
+            // Cast the object back to an array type
+            ItemData[] shopList = data as ItemData[];
+            
+            // Safety Check
+            if (shopList == null || shopList.Length == 0) return;
+            
             shopMainPanel.SetActive(true);
+            CreateShopItems(shopList);
+        }
+    }
+
+    private void CreateShopItems(ItemData[] shopList)
+    {
+        foreach (ItemData shopItemData in shopList)
+        {
+            // Create the button and get the components
+            GameObject shopItem = Instantiate(shopItemPrefab, shopItemPanel.transform);
+            var iconComponent = shopItem.transform.Find("Item Icon").GetComponent<Image>();
+            var nameComponent = shopItem.transform.Find("Item Name").GetComponent<TextMeshProUGUI>();
+            var buttonComponent = shopItem.GetComponent<Button>();
+
+            // Update the data
+            iconComponent.sprite = shopItemData.itemIconAnimated[0];
+            nameComponent.text = shopItemData.itemName;
+            
+            // Add click event listener to button
+            buttonComponent.onClick.AddListener(() =>
+            {
+                ItemInstance newPurchase = new ItemInstance(shopItemData, 1);
+                InventoryManager.Instance.AddItems(newPurchase);
+            });
         }
     }
 }
