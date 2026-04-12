2b4037b7e46469257e85b0646ff0905404c680a1
Eric Nong <ericnonger@gmail.com>
2026-04-12 22:23:47 +0900

    refactor: change `itemContainer` to be more readable and follow S.O.L.I.D

--- Assets/_Project/Prefabs/Environments/Props/ItemContainer.cs
+++ Assets/_Project/Prefabs/Environments/Props/ItemContainer.cs
@@ -1,20 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class ItemContainer : MonoBehaviour, IInteractable
 {
+    [Header("References")] 
+    [SerializeField] private Sprite containerOpenedSprite;
+    private SpriteRenderer _spriteRenderer;
     private Health _health;
+    
+    [Header("Drop Settings")]
+    [SerializeField] private List<ItemDrop> containerContents = new List<ItemDrop>();
+    [SerializeField] private float spreadRadius = 1.5f;
     
     public bool IsOpened { get; private set; }
     public string ChestID { get; private set; }
-    public Sprite containerOpenedSprite;
-    
-    [Header("Chest Contents")]
-    // The list of items to drop
-    public List<ItemDrop> containerContents = new List<ItemDrop>(); 
-    
-    [Header("Drop Settings")]
-    [SerializeField] private float spreadRadius = 1.5f;
     
     [System.Serializable]
     public struct ItemDrop
@@ -25,92 +25,73 @@ public class ItemContainer : MonoBehaviour, IInteractable
     
     public void Awake()
     {
-        if (string.IsNullOrEmpty(ChestID)) GlobalHelper.GenerateUniqueID(gameObject);
         _health = GetComponent<Health>();
+        _spriteRenderer =  GetComponent<SpriteRenderer>();
+        
+        if (string.IsNullOrEmpty(ChestID)) 
+            GlobalHelper.GenerateUniqueID(gameObject);
     }
 
     private void OnEnable()
     {
         if (_health != null)
-        {
             _health.OnDeath += OpenContainer;
-        }
     }
     
     private void OnDisable()
     {
-        if (_health != null)
-        {
+        if (_health != null) 
             _health.OnDeath -= OpenContainer;
-        }
     }
 
-    public bool CanInteract()
-    {
-        return !IsOpened;
-    }
+    public bool CanInteract() => !IsOpened;
     
     public void Interact()
     {
-        if (!CanInteract()) return;
-        OpenContainer();
+        if (CanInteract())
+            OpenContainer();
     }
 
     public void OpenContainer()
     {
-        // If the chest is already opened, do nothing
         if (IsOpened) return;
-        SetOpened(true);
-        
-        if (containerContents == null || containerContents.Count == 0) return;
 
-        // 1. Count how many physical objects to spawn
-        int itemsToDrop = 0;
-        foreach (ItemDrop itemDrop in containerContents)
-        {
-            if (itemDrop.itemDataSo != null) itemsToDrop++;
-        }
+        UpdateVisuals();
+        DropItems();
+    }
+
+    private void UpdateVisuals()
+    {
+        IsOpened = true;
+        if (_spriteRenderer != null && containerOpenedSprite != null)
+            _spriteRenderer.sprite = containerOpenedSprite;
+    }
+
+    private void DropItems()
+    {
+        // Filter out null data and add valid data to list
+        var validDrops = containerContents.Where(d => d.itemDataSo != null).ToList();
+        if (validDrops.Count <= 0) return;
         
-        // 2. Calculate angle between each item
-        float angleStep = 360f / itemsToDrop;
-        float currentAngle = UnityEngine.Random.Range(0f, 360f);
-        
-        foreach (ItemDrop itemDrop in containerContents)
+        float angleStep = 360f / validDrops.Count;
+        float currentAngle = Random.Range(0f, 360f);
+
+        foreach (ItemDrop drop in validDrops)
         {
-            if (itemDrop.itemDataSo == null)
-            {
-                Debug.LogWarning("Chest item is null!");
-                continue;
-            }
-            
-            // 3. Calculate the target position for each item
-            Vector3 spawnPos = transform.position;
-            Vector3 targetPos = CalculateDropPosition(currentAngle);
-            
-            // 2. Spawn the object at the spawnPos
-            GameObject droppedItemObj = Instantiate(itemDrop.itemDataSo.ItemObject, spawnPos, Quaternion.identity);
-            
-            // 3. Check if the droppedItemObj has ItemObject component, if so, initialize it
-            if (droppedItemObj.TryGetComponent(out ItemObject itemObject))
-            {
-                ItemInstance itemInstance = new ItemInstance(itemDrop.itemDataSo, itemDrop.dropAmount); // Create an item instance
-                itemObject.SetItemObject(itemInstance, targetPos);
-            }
-            
-            // 4. Inccrement the angle for the next item
+            SpawnItem(drop, currentAngle);
             currentAngle += angleStep;
         }
     }
 
-    public void SetOpened(bool opened)
+    private void SpawnItem(ItemDrop drop, float angle)
     {
-        IsOpened = opened;
-        if (IsOpened)
+        Vector3 targetPosition = CalculateDropPosition(angle);
+        GameObject droppedItem =  Instantiate(drop.itemDataSo.ItemObject, transform.position, Quaternion.identity);
+
+        if (droppedItem.TryGetComponent(out ItemObject itemObject))
         {
-            if (TryGetComponent(out SpriteRenderer sr))
-            {
-                sr.sprite = containerOpenedSprite;
-            }
+            var instance = new ItemInstance(drop.itemDataSo, drop.dropAmount);
+            itemObject.SetItemObject(instance, targetPosition);
         }
     }
 
@@ -118,14 +99,12 @@ public class ItemContainer : MonoBehaviour, IInteractable
     {
         // 1. Convert angle from degrees to radian
         float angleRad = angleDegrees * Mathf.Deg2Rad;
-        
         // 2. Get the normalized x and y direction
         Vector2 direction = new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad));    
         // 3. Pick a random distance to keep the spread feeling organic
         float distance = UnityEngine.Random.Range(0.5f, spreadRadius);
-        Vector2 finalOffset = direction * distance;
         
         // 4. Return the final position
-        return transform.position + new Vector3(finalOffset.x, finalOffset.y - 0.5f, 0);
-    }
+        // Offset Y slightly to look better in 2D top-down perspective
+        return transform.position + new Vector3(direction.x * distance, (direction.y * distance) - 0.5f, 0);    }
 }
